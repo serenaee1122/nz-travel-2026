8a5ef03385fd4cfeff31352f616a5cb955f6dd0d
serenaee1122 <serenaee1122@gmail.com>
2026-02-26 10:55:14 +1100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg950\cocoartf2868
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 const CACHE_NAME = 'nz-travel-v1';\
+const urlsToCache = ['./', './index.html', './manifest.json'];\
+\
+self.addEventListener('install', event => \{\
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));\
+\});\
+\
+self.addEventListener('fetch', event => \{\
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));\
+\});}
